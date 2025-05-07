24e7acceb0f2a8b74eb2f57000d8be8f766398f7
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-05-07 17:42:10 +0300

    Adds psycopg to dependencies and configure db settings

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bookshop",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
     }
 }
 
