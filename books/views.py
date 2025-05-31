94fd7a89d6deaa30f51057369a23fe3fcc39e706
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-05-31 08:34:29 +0300

    Renames method in ListBooksView

--- books/views.py
+++ books/views.py
@@ -17,10 +17,10 @@ class ListBooksView(ListAPIView):
 
     def get_queryset(self):
         queryset = Book.objects.prefetch_related("publisher", "genres", "authors")
-        queryset = queryset.filter(**self._permitted_params())
+        queryset = queryset.filter(**self._create_filters())
         return queryset
 
-    def _permitted_params(self):
+    def _create_filters(self):
         genre_uuid = self.request.query_params.get("genre", None)
         if genre_uuid is not None:
             return {"genres__uuid__in": self._get_genre_children_uuids(genre_uuid)}
